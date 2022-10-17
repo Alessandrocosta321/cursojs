2d71db49f3b4ab1b204eb460d2b70fa61fc969d4
Alessandro costa <106621394+Alessandrocosta321@users.noreply.github.com>
2022-10-17 16:29:24 -0300

    curso javascript

--- cursojs/aula11/ex008.js
+++ cursojs/aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 80
 console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel >= 60) { //*condição simples
+if (vel > 60) { //*condição simples
     console.log(`Você ultrapassoua velocidade permitida. MULTADO!`)
 }
 console.log(`dirija sempre usando cinto de segurança!`)
--- cursojs/aula11/ex009.js
+++ cursojs/aula11/ex009.js
@@ -1,6 +1,6 @@
-var pais = 'Brasil'
+var pais = 'BRASIL'
 console.log(`Vivendo em ${pais}`);
-if (pais == 'Brasil' ) {
+if (pais == 'brasil' ) {
     console.log(`Você é Brasileiro!`)
 }else {
     console.log(`Você é Estrangeiro!`);
